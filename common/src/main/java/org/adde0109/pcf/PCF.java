84deaf0f8d858b40ae649753411e93ca4de960ed
Dlewis <dlewidad@proton.me>
2025-10-28 21:22:30 -0500

    Updated the proxy protocol to a newer-safer version.
    Proxy protocol now supports HTTP/HTTPS/SOCKS5

---
+++ common/src/main/java/org/adde0109/pcf/PCF.java
@@ -0,0 +1,87 @@
+package org.adde0109.pcf;
+
+import dev.neuralnexus.taterapi.logger.Logger;
+import dev.neuralnexus.taterapi.meta.MetaAPI;
+import dev.neuralnexus.taterapi.meta.Platforms;
+
+import org.adde0109.pcf.forwarding.Mode;
+import org.jetbrains.annotations.ApiStatus;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public final class PCF {
+    private PCF() {}
+
+    public static final String MOD_ID = "pcf";
+    public static final String CONFIG_FILE_NAME = "proxy-compatible-forge.toml";
+
+    private static final PCF INSTANCE = new PCF();
+    public static final Logger logger = Logger.create(MOD_ID);
+
+    public static PCF instance() {
+        return INSTANCE;
+    }
+
+    @ApiStatus.Internal
+    public static void forceLoadConfig() {
+        try {
+            if (MetaAPI.instance().isPlatformPresent(Platforms.FORGE)) {
+                Class.forName("org.adde0109.pcf.v1_14_4.forge.Config")
+                        .getMethod("reload")
+                        .invoke(null);
+            } else if (MetaAPI.instance().isPlatformPresent(Platforms.NEOFORGE)) {
+                Class.forName("org.adde0109.pcf.v1_20_2.neoforge.Config")
+                        .getMethod("reload")
+                        .invoke(null);
+            }
+        } catch (ClassNotFoundException
+                | IllegalAccessException
+                | InvocationTargetException
+                | NoSuchMethodException e) {
+            logger.error("Failed to load Config class", e);
+        }
+    }
+
+    private Forwarding forwarding;
+
+    public Forwarding forwarding() {
+        return this.forwarding;
+    }
+
+    @ApiStatus.Internal
+    public void setForwarding(Forwarding forwarding) {
+        this.forwarding = forwarding;
+    }
+
+    private CrossStitch crossStitch;
+
+    public CrossStitch crossStitch() {
+        return this.crossStitch;
+    }
+
+    @ApiStatus.Internal
+    public void setCrossStitch(CrossStitch crossStitch) {
+        this.crossStitch = crossStitch;
+    }
+
+    private Debug debug;
+
+    public Debug debug() {
+        return this.debug;
+    }
+
+    @ApiStatus.Internal
+    public void setDebug(Debug debug) {
+        this.debug = debug;
+    }
+
+    public record Forwarding(boolean enabled, Mode mode, String secret) {}
+
+    public record CrossStitch(
+            boolean enabled,
+            List<String> forceWrappedArguments,
+            boolean forceWrapVanillaArguments) {}
+
+    public record Debug(boolean enabled, List<String> disabledMixins) {}
+}
---
+++ common/src/main/java/org/adde0109/pcf/mixin/plugin/PCFMixinPlugin.java
@@ -0,0 +1,53 @@
+package org.adde0109.pcf.mixin.plugin;
+
+import dev.neuralnexus.taterapi.muxins.Muxins;
+
+import org.adde0109.pcf.PCF;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+/** A mixin plugin for PCF. */
+@SuppressWarnings("unused")
+public class PCFMixinPlugin implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {}
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    // TODO: Conditionally apply mixins based on enable configs
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        PCF.forceLoadConfig();
+        PCF.Debug debug = PCF.instance().debug();
+        return Muxins.shouldApplyMixin(mixinClassName, debug.disabledMixins(), debug.enabled());
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(
+            String targetClassName,
+            ClassNode targetClass,
+            String mixinClassName,
+            IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(
+            String targetClassName,
+            ClassNode targetClass,
+            String mixinClassName,
+            IMixinInfo mixinInfo) {}
+}
